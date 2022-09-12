8878b80b929c5cfe0fd5e5c810889a72a27e3415
NeoZap <fadhilahazzah04@gmail.com>
2022-09-12 18:57:32 +0700

    Finish pre-deploy part of the assignment

--- katalog/views.py
+++ katalog/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+from katalog.models import CatalogItem
 
-# TODO: Create your views here.
+def show_katalog(request):
+    context = {
+        "item_catalog": CatalogItem.objects.all(),
+        "name": "Aushaaf Fadhilah Azzah",
+        "npm": "2106630063"
+    }
+    return render(request, "katalog.html", context)
